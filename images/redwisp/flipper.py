5c2314701f09e13d96369ab34963e4d98dd4e446
Nithin Muthukumar <33675303+nithinmuthukumar@users.noreply.github.com>
2019-12-25 01:04:11 -0500

    Add files via upload

---
+++ images/redwisp/flipper.py
@@ -0,0 +1,8 @@
+from pygame import*
+from glob import*
+def flip(pics,filename):
+    for i in range(len(pics)):
+        pic=image.load(pics[i])
+        flipic=transform.flip(pic,True,False)
+        image.save(flipic,filename+str(i)+".png")
+flip(glob("left/*.png"),"right")
